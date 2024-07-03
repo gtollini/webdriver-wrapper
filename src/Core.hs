26764970711fc092e79d772fb740f59e730cfe63
Gabriel <gabrieltollini@hotmail.com>
2024-07-03 11:45:21 -0300

    starting Selenium successfully

--- src/Core.hs
+++ src/Core.hs
@@ -2,14 +2,13 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Core (startSelenium, stopSelenium, killSelenium, downloadMissing, testFunction) where
+module Core (startSelenium, stopSelenium, killSelenium, downloadMissing, waitForSeleniumStart) where
 
-import System.Directory (createDirectoryIfMissing, removeFile, doesFileExist)
-import Constants (desiredPlatform, defaultPath, seleniumPath, geckoDriverPath, (</>))
-import System.Process (createProcess, ProcessHandle, cleanupProcess, createProcess_)
-import System.Process.Run (proc, CreateProcess (..))
-import GHC.IO.Handle (Handle, hPutChar, hPutStr)
-import System.Process.Internals (StdStream(..))
+import System.Directory (removeFile, doesFileExist)
+import Constants (defaultPath, seleniumPath, geckoDriverPath, (</>))
+import System.Process (createProcess, ProcessHandle, cleanupProcess)
+import System.Process.Run (proc)
+import GHC.IO.Handle (Handle, hGetLine)
 import System.IO (openFile, IOMode (..))
 import Control.Monad (unless)
 import Data.List (isInfixOf)
@@ -18,22 +17,51 @@ import Data.String.Interpolate (i)
 import Control.Concurrent.Async (concurrently_)
 import Selenium (getSeleniumIfNeeded)
 import GeckoDriver (getGeckoDriverIfNeeded)
+import Control.Retry (constantDelay, limitRetries, RetryStatus (..), retryOnError)
+import System.IO.Error (isEOFError)
+import Control.Monad.Extra (orM)
 
--- I think it always outputs the errors at the file, which might cause some hidden problems.
--- TODO: Check for alternatives.
 startSelenium :: IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
 startSelenium = do
+    defaultPath' <- defaultPath
+    writeFile (defaultPath' </> "seleniumLog.txt") ""
     selPath <- seleniumPath
     geckoPath <- geckoDriverPath
-    defaultPath' <- defaultPath
-    let 
-        selArgs = [[i|-Dwebdriver.gecko.driver=#{geckoPath}|], "-jar", selPath ,"-log", defaultPath' </> "hout"]
+    
+    let
+        logFile = defaultPath' </> "seleniumLog.txt"
+        selArgs = [[i|-Dwebdriver.gecko.driver=#{geckoPath}|], "-jar", selPath ,"-log", logFile]
         processParams = proc "java" selArgs
-    print $ defaultPath' </> "hout"
-    createProcess processParams
+
+    processHandles <- createProcess processParams
+    waitForSeleniumStart logFile
+    return processHandles
 
 downloadMissing :: IO()
 downloadMissing =  concurrently_ getSeleniumIfNeeded getGeckoDriverIfNeeded
+
+waitForSeleniumStart :: String -> IO()
+waitForSeleniumStart logFile = do
+    defaultPath' <- defaultPath
+    fileHandle <-  openFile logFile ReadMode
+
+    succeeded <- retryOnError  retryPolicy'
+        (\_ e -> return $ isEOFError e)
+        (\retryStatus -> if rsIterNumber retryStatus < 599 then logFileHasReadyMessage fileHandle else return False)
+
+    unless succeeded $ error $ "Couldn't start Selenium successfully. Check " ++ (defaultPath' </> logFile)
+
+        where
+            -- wait a minute, retrying every 100ms
+            retryPolicy' = constantDelay 100000 <> limitRetries 600
+
+logFileHasReadyMessage :: Handle -> IO Bool
+logFileHasReadyMessage fileHandle = orM remainingLines
+    where
+        remainingLines = repeat hasReadyMessage
+        hasReadyMessage = (readyMessage `isInfixOf`) <$> hGetLine fileHandle
+
+        readyMessage = "Selenium Server is up and running"
 
 --
 -- functions to help test Selenium and whatnot. Delete later.
@@ -42,27 +70,14 @@ stopSelenium :: (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle) -> IO(
 stopSelenium handles = do
     cleanupProcess handles
     defaultPath' <- defaultPath
-    removeFile (defaultPath' ++ "/hout")
-    removeFile (defaultPath' ++ "/herr")
+    removeFile (defaultPath' </> "seleniumLog.txt")
 
 killSelenium :: IO()
 killSelenium = do
     defaultPath' <- defaultPath
     let
-        herr = defaultPath' ++ "/herr"
-        hout = defaultPath' ++ "/hout"
+        seleniumLogFile = defaultPath' </> "seleniumLog.txt"
         in do
         _ <- createProcess $ proc "fuser" ["4444/tcp", "-k"]
-        herrExists <- doesFileExist herr 
-        houtExists <- doesFileExist hout
-        when herrExists $ removeFile herr
-        when houtExists $ removeFile hout
-
-fullWait :: IO()
-fullWait = do
-    defaultPath' <- defaultPath
-    content1 <- readFile (defaultPath' ++ "/hout")
-    content2 <- readFile (defaultPath' ++ "/herr")
-    unless (readyMessage `isInfixOf` content1 || readyMessage `isInfixOf` content2) fullWait
-    where
-        readyMessage = "Selenium Server is up and running"
+        seleniumLogFileExists <- doesFileExist seleniumLogFile
+        when seleniumLogFileExists $ removeFile seleniumLogFile
