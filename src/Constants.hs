c750c54ea5e509a4531d4ebe08fd0123d6399bef
Gabriel <gabrieltollini@hotmail.com>
2024-07-04 07:04:37 -0300

    haddock for core and constants.

--- src/Constants.hs
+++ src/Constants.hs
@@ -3,6 +3,11 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+{- |
+Module : Constants
+Description : Constant values, such as links and paths.
+ -}
+
 module Constants (geckoDriverPath, defaultPath, defaultSeleniumJarUrl, desiredPlatform, getGeckoDriverDownloadUrl, geckoDriverVersionSource, downloadPath, geckoArchivePath, fileFormat, seleniumPath, seleniumLogPath) where
 
 import Data.String.Interpolate (i)
@@ -11,33 +16,42 @@ import System.Directory (getXdgDirectory, XdgDirectory (XdgData))
 import System.FilePath ((</>))
 
 -- Paths. Should be platform independent.
+-- | @haskell-webdriver-wrapper@ directory, created at `XdgData`. 
 defaultPath :: IO FilePath
 defaultPath = getXdgDirectory XdgData "haskell-webdriver-wrapper"
 
+-- | Directory named after `desiredPlatform`, created at the `defaultPath`.
 downloadPath :: IO FilePath
 downloadPath = (</> desiredPlatform) <$> defaultPath
 
+-- | Intermediary path for the compressed version of geckodriver. Inside `downloadPath`.
 geckoArchivePath :: IO FilePath
 geckoArchivePath = (</> archive) <$> downloadPath
     where
         archive = "geckodriver" <> fileFormat
 
+-- | Path for geckodriver. Inside `downloadPath`. 
 geckoDriverPath :: IO FilePath
 geckoDriverPath = (</> "geckodriver") <$> downloadPath
 
+-- | Path for selenium.jar. Inside `downloadPath`.
 seleniumPath :: IO FilePath
 seleniumPath = (</> "selenium.jar") <$> downloadPath
 
+-- | Path for Selenium's log file. Inside `defaultPath`. 
 seleniumLogPath :: IO FilePath
 seleniumLogPath = (</> "selenium.log") <$> defaultPath
 
 -- URLs. Might change at any moment, kinda why I'm putting them all together here.
+-- | Url to download Selenium from. 
 defaultSeleniumJarUrl :: String
 defaultSeleniumJarUrl = "https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar"
 
+-- | API to get geckodriver's latest version.
 geckoDriverVersionSource :: String
 geckoDriverVersionSource = "https://api.github.com/repos/mozilla/geckodriver/releases/latest"
 
+-- | Url to download geckodriver from. Always the latest version provided by `geckoDriverVersionSource`.
 getGeckoDriverDownloadUrl :: String -> String
 getGeckoDriverDownloadUrl version = [i|https://github.com/mozilla/geckodriver/releases/download/#{version}/geckodriver-#{version}-#{platform}#{format}|]
     where
@@ -45,6 +59,7 @@ getGeckoDriverDownloadUrl version = [i|https://github.com/mozilla/geckodriver/re
         format = fileFormat
 
 -- Platform-dependent variables. 
+-- | Archive format for geckodriver's download. @.zip@ for Windows, @.tar.gz@ for everyone else. 
 fileFormat :: String
 fileFormat = case SI.os of
     "windows" -> ".zip"
@@ -55,6 +70,18 @@ fileFormat = case SI.os of
 
     _         -> ".tar.gz"
 
+-- | Platform this code is running at. The options are:
+-- 
+--      * win64
+--      * win-aarch64
+--      * win32
+--      * macos
+--      * macos-aarch64
+--      * linux64
+--      * linux-aarch64
+--      * linux32
+-- 
+--  If the platform is not identified, @linux64@ is used. 
 desiredPlatform :: String
 desiredPlatform = case (SI.os, SI.arch) of
     ("windows", "x86_64")   -> "win64"
--- src/Core.hs
+++ src/Core.hs
@@ -1,4 +1,10 @@
-module Core (wrappedRunSession, wrappedRunWD, wrapWebDriverFunction) where
+{- |
+Module : Core
+Description : end-user functions.
+
+The wrapped functions (`wrappedRunSession` and `wrappedRunWD`) will download Selenium and Firefox's webdriver (geckodriver) if they're not already on the `Constants.downloadPath`, then start Selenium before running the webdriver equivalent function (`runSession` and `runWD`). They kill the Selenium process at the end of their execution. 
+-}
+module Core (wrappedRunSession, wrappedRunWD, wrapWebDriverFunction, downloadIfMissing) where
 
 import System.Process (cleanupProcess)
 import Control.Concurrent.Async (concurrently_)
@@ -9,12 +15,18 @@ import Test.WebDriver.Types (WD, WDSession)
 import Test.WebDriver.Config (WebDriverConfig)
 import Test.WebDriver.Monad (runWD)
 
+-- | Same as `runSession`, but starts Selenium before execution and kills Selenium after execution. 
+-- Will download Selenium or Firefox's webdriver (geckodriver) if any is missing. 
 wrappedRunSession :: WebDriverConfig conf => conf -> WD a -> IO a
 wrappedRunSession conf wd = wrapWebDriverFunction (conf,wd) (uncurry runSession) 
 
+-- | Same as `runWD`, but starts Selenium before execution and kills Selenium after execution. 
+-- Will download Selenium or Firefox's webdriver (geckodriver) if any is missing. 
 wrappedRunWD ::  WDSession -> WD a -> IO a 
 wrappedRunWD session wd = wrapWebDriverFunction (session, wd) (uncurry runWD)
 
+-- | Runs a function in between starting and killing Selenium. Takes in the arguments and the function, in that order. 
+-- Will download Selenium or Firefox's webdriver (geckodriver) if any is missing. 
 wrapWebDriverFunction :: a -> (a -> IO b) -> IO b
 wrapWebDriverFunction webdriverArgs webdriverFunction = do
     downloadIfMissing
@@ -23,5 +35,6 @@ wrapWebDriverFunction webdriverArgs webdriverFunction = do
     cleanupProcess seleniumHandles
     return b
 
+-- | Dowloads Selenium or Firefox's webdriver (geckodriver) if they're missing. 
 downloadIfMissing :: IO()
 downloadIfMissing =  concurrently_ getSeleniumIfNeeded getGeckoDriverIfNeeded
